9ca70cc8b68f98ad46f6c7669145fe283d1cc794
Bryan Dickens <bigbrd1026@gmail.com>
2024-11-15 20:29:38 -0500

    adding some config info first iteration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://bigbrd.github.io/personal-website-devfolio/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Bryan Dickens',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Bryan Dickens  | Software Engineer`,
     // Description that goes under your name in main bio
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@brdick`,
     // Optional: Github account URL
     github: `https://github.com/ryanfitzgerald`,
     // Optional: LinkedIn account URL
